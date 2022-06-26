3b60773314d9dfca46a849c0fc78c2d0728c7456
ViktorBusk <bviktor228@gmail.com>
2022-06-26 21:20:03 +0200

    Fixed copying new img to old img

--- src/Main.cpp
+++ src/Main.cpp
@@ -48,29 +48,13 @@ void main()
 const char* screenComputeShaderSource = R"(#version 460 core
 layout(local_size_x = 8, local_size_y = 4, local_size_z = 1) in;
 layout(rgba32f, binding = 0) uniform image2D screen;
+layout(rgba32f, binding = 1) uniform image2D screen_old;
+
 void main()
 {
 	vec4 pixel = vec4(0.075, 0.133, 0.173, 1.0);
 	ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
 	
-	ivec2 dims = imageSize(screen);
-	float x = -(float(pixel_coords.x * 2 - dims.x) / dims.x); // transforms to [-1.0, 1.0]
-	float y = -(float(pixel_coords.y * 2 - dims.y) / dims.y); // transforms to [-1.0, 1.0]
-	float fov = 90.0;
-	vec3 cam_o = vec3(0.0, 0.0, -tan(fov / 2.0));
-	vec3 ray_o = vec3(x, y, 0.0);
-	vec3 ray_d = normalize(ray_o - cam_o);
-	vec3 sphere_c = vec3(0.0, 0.0, -5.0);
-	float sphere_r = 1.0;
-	vec3 o_c = ray_o - sphere_c;
-	float b = dot(ray_d, o_c);
-	float c = dot(o_c, o_c) - sphere_r * sphere_r;
-	float intersectionState = b * b - c;
-	vec3 intersection = ray_o + ray_d * (-b + sqrt(b * b - c));
-	if (intersectionState >= 0.0)
-	{
-		pixel = vec4((normalize(intersection - sphere_c) + 1.0) / 2.0, 1.0);
-	}
 	imageStore(screen, pixel_coords, pixel);
 })";
 
@@ -129,6 +113,15 @@ int main()
 	glTextureStorage2D(screenTex, 1, GL_RGBA32F, SCREEN_WIDTH, SCREEN_HEIGHT);
 	glBindImageTexture(0, screenTex, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
+	GLuint screenTexOld;
+	glCreateTextures(GL_TEXTURE_2D, 1, &screenTexOld);
+	glTextureParameteri(screenTexOld, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTextureParameteri(screenTexOld, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTextureParameteri(screenTexOld, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(screenTexOld, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTextureStorage2D(screenTexOld, 1, GL_RGBA32F, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glBindImageTexture(1, screenTexOld, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+
 	GLuint screenVertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(screenVertexShader, 1, &screenVertexShaderSource, NULL);
 	glCompileShader(screenVertexShader);
@@ -184,10 +177,17 @@ int main()
 		glMemoryBarrier(GL_ALL_BARRIER_BITS);
 
 		glUseProgram(screenShaderProgram);
+
 		glBindTextureUnit(0, screenTex);
+
 		glUniform1i(glGetUniformLocation(screenShaderProgram, "screen"), 0);
+
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
+
+        glCopyImageSubData(screenTex,    GL_TEXTURE_2D, 0, 0, 0, 0,
+                           screenTexOld, GL_TEXTURE_2D, 0, 0, 0, 0,
+                           SCREEN_WIDTH, SCREEN_HEIGHT, 1);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
